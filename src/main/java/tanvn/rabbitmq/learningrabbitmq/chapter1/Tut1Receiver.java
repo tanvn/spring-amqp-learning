6073211fe33d4790b35b4c69ba0549285047502b
Vu, Tan | Sam | ECFD <tan.vu@rakuten.com>
2018-06-02 17:00:20 +0900

    first commit

---
+++ src/main/java/tanvn/rabbitmq/learningrabbitmq/chapter1/Tut1Receiver.java
@@ -0,0 +1,13 @@
+package tanvn.rabbitmq.learningrabbitmq.chapter1;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+@RabbitListener(queues = "hello")
+public class Tut1Receiver {
+
+	@RabbitHandler
+	public void receive(String in) {
+		System.out.println(" [x] Received '" + in + "'");
+	}
+}
---
+++ src/test/java/tanvn/rabbitmq/learningrabbitmq/chapter1/RabbitAmqpTutorialsApplicationTests.java
@@ -0,0 +1,17 @@
+package tanvn.rabbitmq.learningrabbitmq.chapter1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitAmqpTutorialsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
