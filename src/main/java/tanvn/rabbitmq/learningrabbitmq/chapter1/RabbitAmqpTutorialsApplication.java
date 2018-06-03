e5c1d4dc3f6eccd95562ba2f561ac100db33f705
Vu Nhat Tan <vunhattan84@gmail.com>
2018-06-03 13:36:30 +0900

    add chapter 2 codes

--- src/main/java/tanvn/rabbitmq/learningrabbitmq/chapter1/RabbitAmqpTutorialsApplication.java
+++ src/main/java/tanvn/rabbitmq/learningrabbitmq/chapter1/RabbitAmqpTutorialsApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ComponentScan("tanvn.rabbitmq.learningrabbitmq.*")
 @SpringBootApplication
 @EnableScheduling
 public class RabbitAmqpTutorialsApplication {
